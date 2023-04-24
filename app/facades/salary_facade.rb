af8d7139af947b8ba0c70e2a80eff50619e35728
Brad Dunlap <braddunlapdpt@gmail.com>
2023-04-24 09:30:57 -0600

    feat: salary facades can pull all job info for tech jobs

--- app/facades/salary_facade.rb
+++ app/facades/salary_facade.rb
@@ -2,6 +2,15 @@
 class SalaryFacade
 	def self.get_salaries(destination)
 		salaries = SalaryService.new.get_salaries(destination)
-		require 'pry'; binding.pry
+		
+		jobs = []
+		job_titles = ["Data Analyst", "Data Scientist", "Mobile Developer", "QA Engineer", "Software Engineer", "Systems Administrator", "Web Developer"]
+		
+		salaries[:salaries].each do |salary|
+			if job_titles.include?(salary[:job][:title])
+				jobs << salary
+			end
+		end
+		jobs
 	end
 end
