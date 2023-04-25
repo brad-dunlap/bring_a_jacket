dc3b88dab3fd43bcfa55b22307d64ae59bf5cabd
Brad Dunlap <braddunlapdpt@gmail.com>
2023-04-25 15:28:13 -0600

    sad path for invalid user

--- spec/requests/api/v0/road_trip_request_spec.rb
+++ spec/requests/api/v0/road_trip_request_spec.rb
@@ -21,6 +21,14 @@ describe 'RoadTrip Api' do
 			}
 		end
 
+		let(:no_api_key) do
+			{
+				"origin": "Cincinatti,OH",
+				"destination": "Chicago,IL",
+				"api_key": "abc123def456"
+			}
+		end
+
 		it 'returns the road trip information if valid params' do
 			post "/api/v0/road_trip", params: request.to_json, headers: { 'CONTENT_TYPE' => 'application/json' }
 			expect(response.status).to eq(200)
@@ -31,7 +39,7 @@ describe 'RoadTrip Api' do
 			expect(results[:data][:attributes].keys).to include(:start_city, :end_city, :travel_time, :weather_at_eta)
 		end
 
-		it 'returns the road trip information if invalid params' do
+		it 'returns the road trip information if invalid travel params' do
 			post "/api/v0/road_trip", params: bad_request.to_json, headers: { 'CONTENT_TYPE' => 'application/json' }
 			expect(response.status).to eq(200)
 
@@ -41,6 +49,14 @@ describe 'RoadTrip Api' do
 			expect(results[:data][:attributes].keys).to include(:start_city, :end_city, :travel_time, :weather_at_eta)
 			expect(results[:data][:attributes][:travel_time]).to eq("impossible")
 			expect(results[:data][:attributes][:weather_at_eta]).to eq({})
+		end
+
+		it 'returns the road trip information if no valid user params' do
+			post "/api/v0/road_trip", params: no_api_key.to_json, headers: { 'CONTENT_TYPE' => 'application/json' }
+			expect(response.status).to eq(401)
+
+			results = JSON.parse(response.body, symbolize_names: true)
+			expect(results[:error]).to eq("Unauthorized")
 		end
 	end
 end
